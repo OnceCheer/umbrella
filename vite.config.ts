debc066864327e32ef566ccfd208e0ec911fe565
145239 <1452396512@qq.com>
2022-02-19 00:36:36 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
-  
-  plugins: [vue()],
+  // plugins: [],
+  plugins: [vue(),pluginRewriteAll()],
   server:{
     host:'0.0.0.0',
     port:8080,
